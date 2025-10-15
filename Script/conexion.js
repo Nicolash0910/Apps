54d119fb1f93ed15b9f1fc2669e94b7a170bcf25
Nicolash0910 <nicolas.ramirezm@uniagustiniana.edu.co>
2025-10-15 22:21:12 +0000

    second commit

--- Script/conexion.js
+++ Script/conexion.js
@@ -1,4 +1,36 @@
-function conexion(){
-    document.getElementById("root").innerHTML = "Conexion"
+async function conexionLista(filtrotipo) {
+
+  
+  if(filtrotipo == "All"){
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${totalPokes}`);
+    const data = await res.json();
+    return data.results;
+  }else{
+    const res = await fetch(`https://pokeapi.co/api/v2/type/${filtrotipo}`);
+    const data = await res.json();
+
+    const pokemonesTipo = [];
+    for (let i = 0; i < data.pokemon.length; i++) {
+      pokemonesTipo.push(data.pokemon[i].pokemon);
+    }
+    return pokemonesTipo;
+  }
 }
 
+let pokemones = [];
+
+async function General() {
+  if (pokemones.length === 0) {
+    pokemones = await conexionLista("All");
+  }
+  Home();
+}
+
+General()
+
+async function FiltroConexion(Elfiltro){
+  document.getElementById("la-lista").innerHTML = "";
+  pokemones = await conexionLista(Elfiltro);
+  const listaHTML = generarLista(pokemones);
+  document.getElementById("la-lista").innerHTML = listaHTML;
+}
